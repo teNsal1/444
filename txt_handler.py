f2665df4e6f337be9edd8b18898c38ffaf687c11
Багаутдинов-Ринат <tensal@bk.ru>
2025-04-19 18:19:26 +0300

    написать и добавить

--- txt_handler.py
+++ txt_handler.py
@@ -5,3 +5,17 @@ class TxtHandler:
                 return file.read()
         except (FileNotFoundError, PermissionError):
             return ""
+
+    def write_file(self, filepath: str, *data: str) -> None:
+        try:
+            with open(filepath, "w", encoding="utf-8") as file:
+                file.write(" ".join(data))
+        except (PermissionError, IsADirectoryError) as e:
+            print(f"Ошибка записи: {e}")
+    
+    def append_file(self, filepath: str, *data: str) -> None:
+        try:
+            with open(filepath, "a", encoding="utf-8") as file:
+                file.write(" ".join(data) + "\n")
+        except (PermissionError, IsADirectoryError) as e:
+            print(f"Ошибка добавления: {e}")
